4e2bec731efac2ff9866db0c0b4ff9fa55081482
Ramazan Çetin <ramazan.cetin@enuygun.com>
2023-05-03 17:56:39 +0300

    update hypnotes-pdf version

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import "hypnotes-pdf/dist/styles/pdf-extension.css"
+import "hypnotes-pdf/dist/styles/pdf-extension.css";
+import "hypnotes-pdf/dist/lib/polyfills";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
